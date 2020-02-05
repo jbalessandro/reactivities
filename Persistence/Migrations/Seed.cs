1f0cbdf673ed06264b37004f0ba58b01558a6bce
jbalessandro <jb.alessandro@gmail.com>
2020-02-04 22:08:00 -0300

    activities

---
+++ Persistence/Migrations/Seed.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+
+namespace Persistence.Migrations
+{
+    public class Seed
+    {
+        public static void SeedData(DataContext context)
+        {
+            if (!context.Activities.Any())
+            {
+                var activities = new List<Activity>
+                {
+                    
+                }
+            }
+        }
+    }
+}
