ccc112c49b54e30f276d072ab97c21a65373ea20
jbalessandro <jb.alessandro@gmail.com>
2020-03-01 21:45:04 -0300

    edit mode

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Grid, GridColumn } from 'semantic-ui-react'
+import { IActivity } from '../../../app/models/activity'
+import ActivityList from './ActivityList'
+import ActivityDetails from '../details/ActivityDetails'
+import ActivityForm from '../form/ActivityForm'
+
+interface IProps {
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
+    editMode: boolean;
+    setEditMode: (editMode: boolean) => void;
+}
+
+const ActivityDashboard: React.FC<IProps> = ({
+    activities, 
+    selectActivity, 
+    selectedActivity,
+    editMode,
+    setEditMode
+}) => {
+    return (
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityList activities={activities} selectActivity={selectActivity}></ActivityList>            
+            </Grid.Column>
+            <GridColumn width={6}>
+                {selectedActivity && !editMode && ( 
+                    <ActivityDetails activity={selectedActivity} setEditMode={setEditMode} />
+                )}
+                {editMode && <ActivityForm />}
+            </GridColumn>
+        </Grid>
+    )
+}
+
+export default ActivityDashboard
